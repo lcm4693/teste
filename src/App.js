a9b3a03dca05bf936553395ebcff2a354576d342
Dan Maldonado <dmaldonado@epc-instore.com>
2020-09-21 22:31:57 -0400

    Initial Component Class

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Diego extends Component {
+   
+   render(){
+      return (
+         <h1>Olá Mundo!</h1>
+      );
+   } 
+   
+};
+
+export Diego;
